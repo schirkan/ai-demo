8d2e346e2d7fe6711cecaee2c00ffeb0c5975d41
schirkan <schirkan@googlemail.com>
2025-06-25 08:10:21 +0000

    style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     <div>
       <h1>AI Demo</h1>
-      <ol>
+      <ol style={{ listStyle: 'decimal', paddingLeft: 'revert' }}>
         <li><a href='chat-ui-sample'>Chat UI Sample</a></li>
         <li><a href='chat-basic'>AI Chat (Basic)</a></li>
         <li><a href='chat-tts'>AI Chat (Voice)</a></li>
